64f2bc625611c2065de79804b2f6b567158fa5d3
Ahmed-Almakki <ahmedlmakkii.com>
2024-10-30 13:28:35 +0000

    use dataset function in the class

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+""" Simplr Pagination """
+import csv
+import math
+from typing import List, Tuple
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def index_range(self, page: int, page_size: int) -> Tuple:
+        """ takes two argument page and page_size
+        Return:
+            a tuple of size two containing start index and end index
+        """
+        start_index = (page - 1) * page_size
+        end_index = start_index + page_size
+        return (start_index, end_index)
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """ uses two argument page and page_size
+            Return:
+                list of lits
+        """
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+        start, end = self.index_range(page, page_size) 
+        data = self.dataset()
+        return data[start:end]
