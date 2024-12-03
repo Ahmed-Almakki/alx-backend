3fa878ead917cc6cce94187857c8c05f2d4d306e
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-12-03 22:23:08 +0000

    promisify and bind and call functions async

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,25 @@
+import { createClient, print } from 'redis';
+
+const client = createClient();
+
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server:', err.toString());
+});
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+const setNewSchool = (schoolName, Value) => {
+  client.SET(schoolName, Value, print);
+};
+
+const displaySchoolValue = (schoolName) => {
+  client.get(schoolName, (err, reply) => {
+    console.log(reply);
+  });
+};
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
